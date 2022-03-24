53ef4eb4dfc12f429453b36d8675fd5c60b26f9a
Asghar Nazir <asghar@syntaxtech.com>
2022-03-24 18:33:23 -0400

    syntax error fix

--- src/test/java/API/AyshaBranch.java
+++ src/test/java/API/AyshaBranch.java
@@ -6,13 +6,13 @@ public class AyshaBranch {
     public static void main(String[] args) {
         System.out.println("Hi this is Aysha branch and I am going to get a career soon");
         System.out.println("I am printing this statement from Henok branch, this is not Aysha branch");
-<<<<<<< HEAD
+
         System.out.println("Adding print statement here because I want to ");
         System.out.println("I did not listen to my team when they told me to pull the latest changes from master");
         System.out.println("I dont know who I think I am");
-=======
+
         System.out.println("Pulling changes from master and will create a pull request");
->>>>>>> 99177feb511083c158d7b69dcd8c5fe3ace2bfa6
+
     }
     
 }
